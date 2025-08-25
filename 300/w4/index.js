b449660748d62e897b39c0ebbcecb915c325edf1
Cody Derbyshire <derbyshire@outlook.co.nz>
2025-08-25 12:29:48 +1200

    finished 300

---
+++ 300/w4/index.js
@@ -0,0 +1,25 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById('myBtn');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  // placeholder
+};
+
+// When the user clicks on <button> (close), close the modal
+span.onclick = function () {
+  // placeholder
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
